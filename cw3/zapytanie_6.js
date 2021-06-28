1c51e3b41b851d084ca95b70734b172a2ce5ccb2
s23787pjwstk <86126413+s23787pjwstk@users.noreply.github.com>
2021-06-28 22:22:18 +0200

    Add files via upload

---
+++ cw3/zapytanie_6.js
@@ -0,0 +1,29 @@
+db.people.insert({
+
+	"sex" : "Male",
+	"first_name" : "Artur",
+	"last_name" : "Jonak",
+	"job" : "DevOps Mng",
+	"email" : "s23787@pjwstk.edu.pl",
+	"location" : {
+		"city" : "Warsaw",
+		"address" : {
+			"streetname" : "Złota",
+			"streetnumber" : "44"
+		}
+	},
+	"description" : "quo vadis",
+	"height" : "193",
+	"weight" : "110",
+	"birth_date" : "1997-01-01T22:35:37Z",
+	"nationality" : "Poland",
+	"credit" : [
+		{
+			"type" : "mastercard",
+			"number" : "123456789012345",
+			"currency" : "PLN",
+			"balance" : "5678.0"
+		}
+	]
+}
+)
